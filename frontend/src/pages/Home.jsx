81230421cc8cd7aa86a8160e71e959010c3dd813
nivia-dev <niviafalves@hotmail.com>
2024-07-02 16:19:12 -0300

    Ignorando node_modules

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -33,7 +33,7 @@ function Home() {
 
     const fetchUfs = async () => {
         try {
-            const response = await axios.get('http://localhost:5000/api/ufs');
+            const response = await axios.get('http://localhost:3000/api/ufs');
             setUfs(response.data);
         } catch (error) {
             console.error('Erro ao buscar UFs', error);
@@ -42,7 +42,7 @@ function Home() {
 
     const fetchMunicipios = async (uf) => {
         try {
-            const response = await axios.get('http://localhost:5000/api/municipios', {
+            const response = await axios.get('http://localhost:3000/api/municipios', {
                 params: { uf }
             });
             setMunicipios(response.data);
@@ -53,7 +53,7 @@ function Home() {
 
     const fetchData = async () => {
         try {
-            const response = await axios.get('http://localhost:5000/api/museus', {
+            const response = await axios.get('http://localhost:3000/api/museus', {
                 params: {
                     pagina,
                     limite,
@@ -69,7 +69,7 @@ function Home() {
 
     const fetchSearchData = async () => {
         try {
-            const response = await axios.get('http://localhost:5000/api/pesquisa', {
+            const response = await axios.get('http://localhost:3000/api/pesquisa', {
                 params: {
                     termo: buscaTermo
                 }
